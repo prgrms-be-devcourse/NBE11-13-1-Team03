836479b3ff889365654068b299236ccc71526716
dlwodRns2 <xoals3088@gmail.com>
2026-07-16 11:04:05 +0900

    coffee-order 프로젝트 초기 설정

---
+++ src/test/java/com/team3/coffee_order/CoffeeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.coffee_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
